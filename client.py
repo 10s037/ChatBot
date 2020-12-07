fb7de8b79529adf34b702d3f2ea7bfbcf0400e03
Daniel Thompson <dthomp33@students.towson.edu>
2020-12-06 20:40:04 -0500

    reformated the file

--- client.py
+++ client.py
@@ -1,30 +1,31 @@
 import socket
 import threading
 
+
 class Client:
     def __init__(self):
         self.create_connection()
 
     def create_connection(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         while 1:
             try:
                 host = input('Enter host name --> ')
                 port = int(input('Enter port --> '))
-                self.s.connect((host,port))
-                
+                self.s.connect((host, port))
+
                 break
             except:
                 print("Couldn't connect to server")
 
         self.username = input('Enter username --> ')
         self.s.send(self.username.encode())
-        
-        message_handler = threading.Thread(target=self.handle_messages,args=())
+
+        message_handler = threading.Thread(target=self.handle_messages, args=())
         message_handler.start()
 
-        input_handler = threading.Thread(target=self.input_handler,args=())
+        input_handler = threading.Thread(target=self.input_handler, args=())
         input_handler.start()
 
     def handle_messages(self):
@@ -33,6 +34,7 @@ class Client:
 
     def input_handler(self):
         while 1:
-            self.s.send((self.username+' - '+input()).encode())
+            self.s.send((self.username + ' - ' + input()).encode())
+
 
 client = Client()
--- server.py
+++ server.py
@@ -1,23 +1,24 @@
 import socket
 import threading
 
+
 class Server:
     def __init__(self):
         self.start_server()
 
     def start_server(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         host = socket.gethostbyname(socket.gethostname())
         port = int(input('Enter port to run the server on --> '))
 
         self.clients = []
 
-        self.s.bind((host,port))
+        self.s.bind((host, port))
         self.s.listen(100)
-    
-        print('Running on host: '+str(host))
-        print('Running on port: '+str(port))
+
+        print('Running on host: ' + str(host))
+        print('Running on port: ' + str(port))
 
         self.username_lookup = {}
 
@@ -25,37 +26,38 @@ class Server:
             c, addr = self.s.accept()
 
             username = c.recv(1024).decode()
-            
-            print('New connection. Username: '+str(username))
-            self.broadcast('New person joined the room. Username: '+username)
+
+            print('New connection. Username: ' + str(username))
+            self.broadcast('New person joined the room. Username: ' + username)
 
             self.username_lookup[c] = username
 
             self.clients.append(c)
-             
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
-    def broadcast(self,msg):
+            threading.Thread(target=self.handle_client, args=(c, addr,)).start()
+
+    def broadcast(self, msg):
         for connection in self.clients:
             connection.send(msg.encode())
 
-    def handle_client(self,c,addr):
+    def handle_client(self, c, addr):
         while True:
             try:
                 msg = c.recv(1024)
             except:
                 c.shutdown(socket.SHUT_RDWR)
                 self.clients.remove(c)
-                
-                print(str(self.username_lookup[c])+' left the room.')
-                self.broadcast(str(self.username_lookup[c])+' has left the room.')
+
+                print(str(self.username_lookup[c]) + ' left the room.')
+                self.broadcast(str(self.username_lookup[c]) + ' has left the room.')
 
                 break
 
             if msg.decode() != '':
-                print('New message: '+str(msg.decode()))
+                print('New message: ' + str(msg.decode()))
                 for connection in self.clients:
                     if connection != c:
                         connection.send(msg)
+
 
 server = Server()
