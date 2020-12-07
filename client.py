72464ff66cf8c8f5131a3a7939cced118b017139
Nathan Russell <70409943+TheGamingArcher@users.noreply.github.com>
2020-12-06 20:42:00 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -1,40 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 22:09:18 2020
+
+@author: Nathan Russell
+"""
+
 import socket
 import threading
-
+import pyDH
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
 
 class Client:
     def __init__(self):
         self.create_connection()
 
     def create_connection(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        
         while 1:
             try:
                 host = input('Enter host name --> ')
                 port = int(input('Enter port --> '))
-                self.s.connect((host, port))
-
+                self.s.connect((host,port))
+                
                 break
             except:
                 print("Couldn't connect to server")
 
         self.username = input('Enter username --> ')
         self.s.send(self.username.encode())
-
-        message_handler = threading.Thread(target=self.handle_messages, args=())
+        
+        message_handler = threading.Thread(target=self.handle_messages,args=())
         message_handler.start()
 
-        input_handler = threading.Thread(target=self.input_handler, args=())
+        input_handler = threading.Thread(target=self.input_handler,args=())
         input_handler.start()
 
     def handle_messages(self):
         while 1:
-            print(self.s.recv(1204).decode())
-
+            
+            cipher = AES.new(key, AES.MODE_CBC, IV) 
+            decrypted = (cipher.decrypt(self.s.recv(1204).decode()), AES.block_size)
+            print(decrypted)
+            
     def input_handler(self):
         while 1:
-            self.s.send((self.username + ' - ' + input()).encode())
-
+            message = input()
+            plaintext = AES.new(key, AES.MODE_CBC, IV)
+            encrypted = (plaintext.encrypt(message, AES.block_size))
+            self.s.send((self.username+' - '+encrypted.encode())
 
 client = Client()
